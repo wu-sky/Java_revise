9dfd4f80595b01aab63e18df11641022d94fafcd
wushikai <Wu_sky@foxmail.com>
2020-05-26 09:11:18 +0800

    String编码解码

---
+++ src/main/java/com/sky/str/StringRevise.java
@@ -0,0 +1,36 @@
+package com.sky.str;
+
+import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
+
+/**
+ * @author 吴世凯
+ * 邮箱：
+ * 日期：5/26/2020
+ */
+public class StringRevise {
+
+
+    public static void codeStr() throws UnsupportedEncodingException {
+
+        String str1=new String("123abc伍思凯");
+        System.out.println("str1.getBytes(utf-8) = " + Arrays.toString(str1.getBytes()));
+        System.out.println("str1.getBytes(gbk) = " + Arrays.toString(str1.getBytes("gbk")));
+        byte[] bytes={49, 50, 51, 97, 98, 99, -28, -68, -115, -26, -128, -99, -27, -121, -81};
+        String str2=new String(bytes);
+        System.out.println("str2 = " + str2);
+        bytes=new byte[]{49, 50, 51, 97, 98, 99, -50, -23, -53, -68, -65, -83};
+        str2=new String(bytes, "gbk");
+        System.out.println("str2 = " + str2);
+
+    }
+
+
+    public static void main(String[] args) throws UnsupportedEncodingException {
+        codeStr();
+    }
+
+
+
+
+}
